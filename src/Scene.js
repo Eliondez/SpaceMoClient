47b76b68b203138484f9f82488f686f7e09c02a7
Elion <dz_elf@mail.ru>
2017-11-26 11:17:55 +0300

    массовый рефакторинг, убрали всё в функции, трём глобальные переменные!

--- src/Scene.js
+++ src/Scene.js
@@ -35,22 +35,18 @@ var Scene = function() {
     return key;
   }
 
+
   var left = keyboard(65),
       up = keyboard(87),
       right = keyboard(68),
       down = keyboard(83),
       fireBtn = keyboard(32);
   
-  var sprite,
-      state,
+  var state,
       // turret,
       centerLine,
-      hull,
-      bgCont,
-      stars1cont,
-      stars2cont,
       enemyList,
-      bulletList;
+      bulletList = {};
 
   var Container = PIXI.Container,
       autoDetectRenderer = PIXI.autoDetectRenderer,
@@ -77,17 +73,60 @@ var Scene = function() {
 
   
   function setup() {
-    var id = resources["http://localhost:8080/public/ships.json"].textures;
-    bgCont = new Container();
-    stars1cont = new Container();
+    createBg();
+    var player_ship = createUser();
+    bindKeys(player_ship);
+    createEnemies();
+    stage.checkCollision = function(ent1, ent2) {
+      var colDist = 20;
+      var dist = Math.hypot(ent1.x - ent2.x, ent1.y - ent2.y);
+      if (dist < colDist) {
+        return true;
+      }
+      return false;
+    }
+    state = play;
+    renderer.render(stage);
+    gameLoop();
+  };
+  function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    state();
+    renderer.render(stage);
+  }
+
+  function play() {
+    for (var i = stage.children.length - 1; i >= 0; i-- ) {
+     stage.children[i].update();
+    }
+        for (var i in bulletList) {
+      for (var j in enemyList) {
+        // if (stage.checkCollision(bulletList[i], enemyList[j])) {
+        //   bulletList[i].toDestroy = true;
+        //   enemyList[j].toDestroy = true;
+        // }
+      }
+    }
+  }
+
+  var deleteAll = function() {
+    bulletList = [];
+    enemyList = [];
+    for (var i = stage.children.length - 1; i >= 0; i-- ) {
+      stage.removeChild(stage.children[i]);
+    }
+  }
+
+  var createBg = function() {
+    var bgCont = new Container();
+    var stars1cont = new Container();
     var stars11 = new Sprite(resources["http://localhost:8080/public/stars1.png"].texture);
     stars11.position.set(0, 0);
     stars1cont.addChild(stars11);
     var stars12 = new Sprite(resources["http://localhost:8080/public/stars1.png"].texture);
     stars12.position.set(0, -800);
     stars1cont.addChild(stars12);
-
-    stars2cont = new Container();
+    var stars2cont = new Container();
     var stars21 = new Sprite(resources["http://localhost:8080/public/stars2.png"].texture);
     stars21.position.set(0, 0);
     stars2cont.addChild(stars21);
@@ -96,69 +135,79 @@ var Scene = function() {
     stars2cont.addChild(stars22);
     bgCont.addChild(stars1cont);
     bgCont.addChild(stars2cont);
-    stage.addChild(bgCont);
-    
-    enemyList = [];
-    bulletList = [];
-    var enemyNum = 15;
-    var enemySpan = 50;
-    var centerPosition = 500;
-    var enemyWide = (enemyNum - 1) * enemySpan;
-    for (var i = 0; i < enemyNum; i++) {
-      var id = resources["http://localhost:8080/public/ships.json"].textures;
-      var enemy = new Sprite(id["greenship3.png"]);
 
-      sprite = new Container();
-      enemy.position.set(centerPosition - enemyWide / 2 + i * 50, 100);
-      enemy.rotation = Math.PI;
-      enemy.scale.x = 0.2;
-      enemy.scale.y = 0.2;
-      stage.addChild(enemy);
-      enemyList.push(enemy);
+    bgCont.update = function() {
+      stars1cont.y += 0.5;
+      if (stars1cont.y > 800) {
+        stars1cont.y = 0;
+      }
+      stars2cont.y += 0.8;
+      if (stars2cont.y > 800) {
+        stars2cont.y = 0;
+      }
     }
 
-    
-    hull = new Sprite(id["orangeship3.png"]);
+    stage.addChild(bgCont);
+  }
+
+  var createUser = function() {
+    var id = resources["http://localhost:8080/public/ships.json"].textures;
+    var hull = new Sprite(id["orangeship3.png"]);
     hull.anchor.set(0.5, 0.5);
     hull.position.set(0, 0);
     hull.scale.x = 0.3;
     hull.scale.y = 0.3;
-
-    stage.checkCollision = function(ent1, ent2) {
-      var colDist = 20;
-      var dist = Math.hypot(ent1.x - ent2.x, ent1.y - ent2.y);
-      if (dist < colDist) {
-        console.log("Collision!");
-        return true;
-      }
-      return false;
-    }
-
-    sprite = new Container();
+    var sprite = new Container();
     sprite.position.set(500, 550);
     sprite.vx = 0;
     sprite.vy = 0;
     sprite.pewpew = false;
     sprite.reload = 0;
     sprite.reloadMax = 14;
-    sprite.linearVel = 1.5; 
+    sprite.linearVel = 1.5;
+    sprite.update = function() {
+      sprite.x += sprite.vx;
+      sprite.y += sprite.vy;
+            
+      if (sprite.reload > 0) {
+        sprite.reload -= 1;
+      }
+
+      if (sprite.pewpew && sprite.reload === 0) {
+        sprite.shoot();
+        sprite.reload = sprite.reloadMax;
+      }
+    } 
     sprite.shoot = function() {
       var bullet = new PIXI.Graphics();
+      bullet.id = Math.random();
       bullet.beginFill(0xffc107, 1);
       bullet.drawRect(-2, -4, 4, 8);
       bullet.endFill();
-      // bullet.anchor.set(0.5, 0.5);
       bullet.position.set(sprite.x, sprite.y - 30);
       bullet.lifetime = 250;
-      bulletList.push(bullet);
+      bullet.update = function() {
+        bullet.y -= 3;
+        bullet.lifetime -= 1;
+        if (bullet.lifetime <= 0) {
+          bullet.toDestroy = true;
+        }
+        if (bullet.toDestroy) {
+          bullet.destroy();
+          delete bulletList[bullet.id];
+        }
+      };
+
+      bulletList[bullet.id] = bullet;
       stage.addChild(bullet);
-      // hull.scale.x = 0.3;
-      // hull.scale.y = 0.3;
     }
     sprite.addChild(hull);
-    // sprite.addChild(turret);
     stage.addChild(sprite);
 
+    return sprite;
+  }
+
+  var bindKeys = function(sprite) {
     up.press = function() {
       sprite.vy -= sprite.linearVel;
     };
@@ -200,79 +249,32 @@ var Scene = function() {
     fireBtn.release = function() {
       sprite.pewpew = false;
     }
-    state = play;
-    renderer.render(stage);
-    gameLoop();
-  };
-  function gameLoop() {
-    requestAnimationFrame(gameLoop);
-    state();
-    
-    renderer.render(stage);
-
-    for (var j in enemyList) {
-      enemyList[j].y += 0.5;
-      if (enemyList[j].toDestroy) {
-        enemyList[j].destroy();
-        enemyList.splice(j, 1);
-      }
-    }
-
-
-    for (var i in bulletList) {
-      bulletList[i].y -= 3;
-      bulletList[i].lifetime -= 1;
-      if (bulletList[i].toDestroy) {
-        bulletList[i].destroy();
-        bulletList.splice(i, 1);
-      }
-    }
   }
 
-  function play() {
-    // turret.rotation += turret.vRot * 0.05;
-    sprite.x += sprite.vx;
-    sprite.y += sprite.vy;
-    stars1cont.y += 0.5;
-    if (stars1cont.y > 800) {
-      stars1cont.y = 0;
-    }
-    stars2cont.y += 0.8;
-    if (stars2cont.y > 800) {
-      stars2cont.y = 0;
-    }
-    if (sprite.reload > 0) {
-      sprite.reload -= 1;
-    }
-
-    if (sprite.pewpew && sprite.reload === 0) {
-      sprite.shoot();
-      sprite.reload = sprite.reloadMax;
-    }
-
-    for (var i in bulletList) {
-      for (var j in enemyList) {
-        if (stage.checkCollision(bulletList[i], enemyList[j])) {
-          bulletList[i].toDestroy = true;
-          enemyList[j].toDestroy = true;
+  var createEnemies = function() {
+    enemyList = {};
+    var enemyNum = 15;
+    var enemySpan = 50;
+    var centerPosition = 500;
+    var enemyWide = (enemyNum - 1) * enemySpan;
+    for (var i = 0; i < enemyNum; i++) {
+      var id = resources["http://localhost:8080/public/ships.json"].textures;
+      var enemy = new Sprite(id["greenship3.png"]);
+      var sprite = new Container();
+      enemy.id = Math.random();
+      enemy.position.set(centerPosition - enemyWide / 2 + i * 50, 100);
+      enemy.rotation = Math.PI;
+      enemy.scale.x = 0.2;
+      enemy.scale.y = 0.2;
+      enemy.update = function() {
+        enemy.y += 0.5;
+        if (enemy.toDestroy) {
+          enemy.destroy();
+          delete enemyList[enemy.id];
         }
       }
-    }
-    for (var i in bulletList) {
-      bulletList[i].y -= 3;
-      bulletList[i].lifetime -= 1;
-      if (bulletList[i].lifetime <= 0) {
-        bulletList[i].toDestroy = true;
-      }
-    }
-
-  }
-
-  var deleteAll = function() {
-    bulletList = [];
-    enemyList = [];
-    for (var i = stage.children.length - 1; i >= 0; i-- ) {
-      stage.removeChild(stage.children[i]);
+      stage.addChild(enemy);
+      enemyList[enemy.id] = enemy;
     }
   }
 
@@ -281,12 +283,10 @@ var Scene = function() {
   }
 
   self.reload = function() {
-    // state = null;
     deleteAll();
-    // createBg();
-    // createUser();
-    // createEnemies();
-    // state = play;
+    createBg();
+    createUser();
+    createEnemies();
   }
   return self;
 }
