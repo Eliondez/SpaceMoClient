d7a867339adc45201e4e04bcea4659c35b3fde0d
Eliondez <dz_elf@mail.ru>
2017-12-05 14:38:00 +0300

    враги перемещаются в случайных направлениях

--- src/Scene.js
+++ src/Scene.js
@@ -235,7 +235,8 @@ var Scene = function() {
     sprite.shoot = function() {
       Bullet({
         x: sprite.x,
-        y: sprite.y - 30
+        y: sprite.y,
+        angle: sprite.rotation
       })
     }
 
@@ -308,6 +309,8 @@ var Scene = function() {
     // b_sprite.anchor.set(0.5, 0.5);
 
     self.sprite = b_sprite;
+    // self.sprite.zOrder = 100;
+    console.log(self.sprite.zOrder);
     self.sprite.update = function() {
       self.sprite.y += self.yVel;
       self.sprite.x += self.xVel;
@@ -335,12 +338,43 @@ var Scene = function() {
     self.sprite.scale.x = options.scale || 0.2;
     self.sprite.scale.y = options.scale || 0.2;
     self.sprite.anchor.set(0.5, 0.5);
+
+    self.vx = 0;
+    self.vy = 0;
+    self.targetPos = {
+      x: options.x,
+      y: options.y
+    }
+    self.linearVel = 0.5;
+
     self.sprite.update = function() {
-      self.sprite.y += 0.5;
+
+      var dist = Math.hypot(self.sprite.x - self.targetPos.x, self.sprite.y - self.targetPos.y);
+      if (dist > 5) {
+        self.sprite.x += self.vx;
+        self.sprite.y += self.vy;
+      } else {
+        self.moveTo({
+          x: self.sprite.x + Math.random() * 200 - 100,
+          y: self.sprite.y + Math.random() * 200 - 100
+        })
+      }
+
+      self.sprite.y += self.vy;
+      self.sprite.x += self.vx;
+
       if (self.sprite.toDestroy) {
         self.sprite.destroy();
         delete enemyList[self.id];
       }
+    }
+    self.moveTo = function(target) {
+      self.targetPos.x = target.x;
+      self.targetPos.y = target.y;
+      var angle = Math.atan2(self.targetPos.x - self.sprite.x, self.sprite.y - self.targetPos.y);
+      self.sprite.rotation = angle
+      self.vx = Math.sin(angle) * self.linearVel;
+      self.vy = -Math.cos(angle) * self.linearVel;
     }
     stage.addChild(self.sprite);
     enemyList[self.id] = self.sprite;
@@ -355,8 +389,8 @@ var Scene = function() {
     var enemyWide = (enemyNum - 1) * enemySpan;
     for (var i = 0; i < enemyNum; i++) {
       Enemy({
-        x: centerPosition - enemyWide / 2 + i * 50,
-        y: 100
+        x: 100 + Math.random() * (renderer.width - 200) ,
+        y: 100 + Math.random() * (renderer.height - 200)
       })
     }
   }
